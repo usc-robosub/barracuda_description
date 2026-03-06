593685c35f2ef03f55b38f846446868c887903fe
Takatoshi Soeda <toshi-yondu>
2026-03-06 13:00:12 -0800

    Flip thruster orientations and add joint state publisher with GUI dependencies

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -35,6 +35,14 @@ def generate_launch_description():
         parameters=[params],
     )
 
+    node_joint_state_publisher = Node(
+        package="joint_state_publisher",
+        namespace="barracuda",
+        executable="joint_state_publisher",
+        output="screen",
+        parameters=[{"use_sim_time": use_sim_time}],
+    )
+
     # Launch!
     return LaunchDescription(
         [
@@ -43,6 +51,7 @@ def generate_launch_description():
                 default_value="false",
                 description="Use sim time if true",
             ),
+           node_joint_state_publisher,
            node_robot_state_publisher,
         ]
     )
